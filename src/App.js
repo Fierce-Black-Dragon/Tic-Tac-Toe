e3b4634008e28220fc1963977fbb64902e309399
Fierce-Black-Dragon <blackdragon4204@gmail.com>
2020-09-30 17:39:14 +0530

    added tic tac toe

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const App = () =>{
+
+ return(
+
+  <>
+  <h1>Welcome to React Parcel Micro App!</h1>
+  <p>Hard to get more minimal than this React app.</p>
+  <p>helloooooo??????</p>
+ </>
+ );
+
+
+};
+export default App
