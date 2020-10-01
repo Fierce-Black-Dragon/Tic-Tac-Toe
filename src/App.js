c5d64539476872093560acd93a814d300074f1cb
Fierce-Black-Dragon <blackdragon4204@gmail.com>
2020-10-01 18:54:48 +0530

    added sass  to the project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Board from './Components/Board'
 
+import "./Styles/root.scss"
+
 const App = () =>{
 
  return(
 
-  <div>
+  <div className='app'>
   
   <p>TIC TAC TOE</p>
   <Board/>
