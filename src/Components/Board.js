9ff7dc39712c23876bff99e8365decb270ca4ff0
Fierce-Black-Dragon <blackdragon4204@gmail.com>
2020-10-01 20:55:11 +0530

    logic added with hooks

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -3,45 +3,50 @@ import Sqaure from './Sqaure'
 
 const Board = () => {
     const [board,setBoard]=useState(Array(9).fill(null));
-    const [xNext ,setXNext]=useState(false);
-    const handleSqClick = (position) =>{
+    const [isXNext ,setIsXNext]=useState(false);
+    const handeClick=(position)=>{
         if(board[position]){
             return;
         }
-     setBoard((prev) => {
-        return prev.map((Sqaure, pos)=>{
-            if(pos === position){
-                return  xNext? 'X': 'O';
-         }
-         return Sqaure;
-        });
-     
-     });
-     setXNext((prev)=> !prev)
-    };
-    const renderSq =(position) =>{
-        return  <Sqaure value={board[position]} squareClick={()=>{handleSqClick(position)}}/>
-    };
-    return (
-        
-        <div className="board">
-           <div className="board-row ">
-              {renderSq(0)}
-              {renderSq(1)}
-              {renderSq(2)}
+        setBoard(prev=>{
+               return prev.map((Sqaure, pos)=>{
+                   if(pos === position){
+                       return  isXNext ? 'X' : 'O';
 
+                   }
+                   return Sqaure;
+               });
+               
+           });
+        setIsXNext( (prev) => !prev );
+    };
+    const renderSq= (position) =>{
+       return (
+       <Sqaure
+       value={board[position]} 
+       squareClick={()=>handeClick(position)}/>
+       );
+       
+    };
+  
+    return (
+        <div className="board">
+            <div className="board-row ">
+             {renderSq(0)}
+             {renderSq(1)}
+             {renderSq(2)}
+            
             </div >
             <div className="board-row ">
-             {renderSq(3)}
-             {renderSq(4)}
-             {renderSq(5)}
+            {renderSq(3)}
+            {renderSq(4)}
+            {renderSq(5)}
             </div>
             <div className="board-row ">
-             {renderSq(6)}
-             {renderSq(7)}
-             {renderSq(8)}
-
-        </div>
+            {renderSq(6)}
+            {renderSq(7)}
+            {renderSq(8)}
+            </div>
         </div>
     )
 }
